e8f3a8f6018a0f231620e90e1c40ef3b34109d11
erik tolentino and yvonna contreras <yvonnap.contreras@gmail.com>
2016-04-05 16:31:45 -0700

    Add styling

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ Router.map(function() {
   this.route('results', {path: '/results/:zip'});
   this.route('billresults', {path: 'billresults/:keyword'});
   this.route('bill', {path: 'bill/:billId'});
+  this.route('about');
+  this.route('contact');
 });
 
 export default Router;
